b58928391d3ffd0bd1b28266b278ae82c9c6010d
William Li <w577li@uwaterloo.ca>
2023-08-08 08:44:01 +0800

    -- will 0807 interaction

--- interaction-service/src/main/java/com/abx/interactionservice/model/User.java
+++ interaction-service/src/main/java/com/abx/interactionservice/model/User.java
@@ -1,28 +1,15 @@
 package com.abx.interactionservice.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
     private UUID userUuid;
 
     // Other fields, getters, setters, and constructors
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public UUID getUserUuid() {
         return userUuid;
